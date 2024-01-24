ee202fb0c03cd6bdae6448f569390eb6f7015e06
ST10082757 <ST10082757@vcconnect.edu.za>
2024-01-24 12:10:26 +0200

    Add project files.

---
+++ Calculator/MainWindow.xaml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Calculator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int first;
+        int second;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+    
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+
+            first = Int32.Parse(txt1.Text);
+            second = Int32.Parse(txt2.Text);
+            // operators.SelectedItem is ComboBoxItem selectedItem
+            if (operators.SelectedIndex ==0)
+            {
+                txtAns.Text = (first + second).ToString();
+            }
+            else if (operators.SelectedIndex ==1) 
+            {
+                txtAns.Text = (first - second).ToString();
+
+            }
+            else if (operators.SelectedIndex == 2)
+            {
+                txtAns.Text = (first * second).ToString();
+
+            }
+            else if(operators.SelectedIndex == 3)
+            {
+                txtAns.Text = (first / second).ToString();
+
+            }
+            
+        }
+    }
+}
---
+++ CalculatorLibrary/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorLibrary
+{
+    public class Class1
+    {
+        public static class CalculatorValidator
+        {
+            public static bool ValidateNumber(string input)
+            {
+                return double.TryParse(input, out _);
+            }
+
+        }
+    }
+}
